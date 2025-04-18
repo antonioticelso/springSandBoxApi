9595f92049501481aa12a84a4921298fd396bf54
Antonio <antonio.ti.celso@gmail.com>
2025-04-18 13:35:02 -0300

    inicio do progeto

---
+++ src/main/java/com/github/antonioticelso/springSandBoxApi/SpringSandBoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.springSandBoxApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandBoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSandBoxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/antonioticelso/springSandBoxApi/SpringSandBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.springSandBoxApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSandBoxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
